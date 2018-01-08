9af0352db3adadbeb84ff697c52f17188b075732
hkyupelyan <hkyupelyan@WINDOWS2016-DEV>
2018-01-07 16:01:08 -0800

    added a comment to trigger the build

--- MvcTestDeployApp/Models/Thing.cs
+++ MvcTestDeployApp/Models/Thing.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace MvcTestDeployApp.Models
 {
+    /// <summary>
+    /// just added a comment to see if polling will trigger with commit
+    /// </summary>
     public class Thing
     {
         public long ThingId { get; set; }
