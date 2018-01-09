20e9b8042c863b4dbaee36c0c1963571aec47e37
hkyupelyan <hkyupelyan@WINDOWS2016-DEV>
2018-01-08 22:02:26 -0800

    updated everything for description attribute

--- MvcTestDeployApp/Controllers/ThingsController.cs
+++ MvcTestDeployApp/Controllers/ThingsController.cs
@@ -46,7 +46,7 @@ namespace MvcTestDeployApp.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ThingId,Name")] Thing thing)
+        public ActionResult Create([Bind(Include = "ThingId,Name,Description")] Thing thing)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace MvcTestDeployApp.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ThingId,Name")] Thing thing)
+        public ActionResult Edit([Bind(Include = "ThingId,Name,Description")] Thing thing)
         {
             if (ModelState.IsValid)
             {
